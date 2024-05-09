79aa0e5e35dfb4a1a1cedee0dae556077c179854
gauravgb5 <gbawaskar18@gmail.com>
2024-05-09 13:13:36 +0530

    msg method add in dev1.java file

--- git1/src/git1/dev1.java
+++ git1/src/git1/dev1.java
@@ -4,4 +4,9 @@ public class dev1 {
 	public static void main(String[] args) {
 		System.out.println("Dev1 class in git1 project");
 	}
+	
+	void msg() {
+		System.out.println("dev1 method");
+	}
+	
 }
